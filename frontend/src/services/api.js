5f9351cb3bc349bf1abb94fb970f2c1e1972f8ca
Torix12 <georgetormanovi@gmail.com>
2024-12-16 19:01:21 +0400

    Testing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 // Dynamically set the backend URL based on the environment
-const API_BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://your-deployed-backend-domain.com/index.php' // Replace with your deployed backend URL
-    : 'http://localhost:8000/frontend/index.php'; // Local development URL
-
+const API_BASE_URL = 'https://shopping-cart-vert-nine.vercel.app'
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
